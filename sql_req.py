f8bb9b99bfe210f4ee40b01c5ea52e6bd1eb1188
Slovvturtle <11a447273>
2020-03-31 20:39:23 +0300

    Signed-off-by: Slovvturtle <11a447273>

--- sql_req.py
+++ sql_req.py
@@ -21,18 +21,17 @@ def add_film(title, genre, studio, session, director):
 
 
 def add_viewer(name):
-    return Studio.create(name = name)
+    return Viewer.create(name = name)
 
 
 def add_cinema(title, address):
-    return Studio.create(title = title, address = address)
+    return Cinema.create(title = title, address = address)
 
 
 if __name__ == '__main__':
-    # OPO_GS= Session.create(date = date(2019, 1, 10) , time = time(12, 35), duration = 2)
-    #  director = add_director(name = 'Peter Farrelly', age = 63)
-    #  studio = add_studio(title = 'Universal Pictures', budget= 23000000)
-    #  session = add_session(date = date(2019,1,24), time = time(15,45), duration = 2)
-    #  film = add_film(title='Green Book', genre=' biographical comedy-drama', studio= studio.title, session= session.time, director= director.name)
-    viewer = add_viewer(name = 'Peter Johnson')
-    cinema = add_cinema(title= 'Cinema Village', address = '22 E 12th St, New York')
+   film = add_film(title='Green Book', genre=' biographical comedy-drama', studio=  'Universal Pictures' , session= time(15, 45) , director= 'Peter Farrelly')
+   director = add_director(name= film.director, age=63)
+   studio = add_studio(title=film.studio, budget=23000000)
+   session = add_session(date=date(2019, 1, 24), time= film.session, duration=2)
+   viewer = add_viewer(name = 'Peter Johnson')
+   cinema = add_cinema(title= 'Cinema Village', address = '22 E 12th St, New York')
