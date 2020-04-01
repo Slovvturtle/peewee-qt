98d6dda498b318aa0232cefd87aab5d6e60654d7
Slovvturtle <11a447273>
2020-04-01 18:16:12 +0300

    Signed-off-by: Slovvturtle <11a447273>

--- sql_req.py
+++ sql_req.py
@@ -32,7 +32,8 @@ if __name__ == '__main__':
    director = add_director(name= 'Peter Farrelly', age=63)
    studio = add_studio(title= 'Universal Pictures', budget=23000000)
    session = add_session(date=date(2019, 1, 24), time= time(15, 45), duration=2)
-   film = add_film(title='Green Book', genre=' biographical comedy-drama', studio= studio.title , session= session.time, director= director.name)
+   film = add_film(title='Green Book', genre=' biographical comedy-drama', studio= studio , session= session, director=director)
    viewer = add_viewer(name = 'Peter Johnson')
    cinema = add_cinema(title= 'Cinema Village', address = '22 E 12th St, New York')
-   
+   for director in Director.select():
+    print(director.name, Director.age)
